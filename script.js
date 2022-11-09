512519a4cc484e253a3745741f0288b84b6586a8
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-09 12:40:00 +0100

    myrorna och cicada inlagt

---
+++ script.js
@@ -0,0 +1,13 @@
+const works ={
+    cicada: document.getElementById("cicada"),
+    myrorna: document.getElementById("myrorna")
+}
+works.cicada.addEventListener("click",function(website){
+    window.location.href = "cicada/index.html";
+})
+works.myrorna.addEventListener("click",function(website){
+    window.location.href = "myrorna-vintage/index.html";
+})
+works.cicada.addEventListener("onhover", function(){
+   works.cicada.style.border="5px solid red";
+});
